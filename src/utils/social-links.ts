a2e69da50b473ba522f65ad64a49790674b3523b
cinagroup <info@cinagroup.com>
2026-08-29 22:52:30 +0800

    chore: rename repository to cinagroup

--- src/utils/social-links.ts
+++ src/utils/social-links.ts
@@ -2,7 +2,7 @@
 
 export const SOCIAL_LINKS = {
   x: 'https://x.com/cinagroup',
-  github: 'https://github.com/cinagroup/homepage',
+  github: 'https://github.com/cinagroup/cinagroup',
   instagram: '#',
   facebook: '#',
   rss: '/rss.xml',
